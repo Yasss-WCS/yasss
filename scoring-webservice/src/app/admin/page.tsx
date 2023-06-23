874ed5e5ca126aed30722d0708a0c95b6105db02
Alec Grant <agrant@caseguard.com>
2023-06-23 12:52:20 -0400

    Add mui and emotion

--- scoring-webservice/src/app/admin/page.tsx
+++ scoring-webservice/src/app/admin/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+import * as mui from '@mui/material';
+import * as muiIcons from '@mui/icons-material'
+
 const Competition = {
     name: "Liberty Swing 2023",
     division: "Novice Jack & Jill",
@@ -5,17 +9,33 @@ const Competition = {
 
 function CompetitionCard() {
     return (
-        <div>
+        <mui.Card variant="outlined">
             <div>Competition:</div>
             <div>{Competition.name}</div>
             <div>{Competition.division}</div>
-        </div>
+        </mui.Card>
+    )
+}
+
+function Header() {
+    return (
+        <mui.AppBar position="static">
+            <mui.Toolbar variant="dense">
+                <mui.IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <muiIcons.Menu/>
+                </mui.IconButton>
+                <mui.Typography variant="h6" color="inherit" component="div">
+                    {Competition.name}
+                </mui.Typography>
+            </mui.Toolbar>
+        </mui.AppBar>
     )
 }
 
 export default function admin() {
     return (
     <main>
+        <Header/>
         <CompetitionCard/>
     </main>
     )
