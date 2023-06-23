5060a151e74b31405f487fb740e12a13d1880c2c
Alec Grant <agrant@caseguard.com>
2023-06-22 21:32:11 -0400

    Add test for card on admin panel

--- scoring-webservice/src/app/admin/page.tsx
+++ scoring-webservice/src/app/admin/page.tsx
@@ -1,7 +1,22 @@
+const Competition = {
+    name: "Liberty Swing 2023",
+    division: "Novice Jack & Jill",
+};
+
+function CompetitionCard() {
+    return (
+        <div>
+            <div>Competition:</div>
+            <div>{Competition.name}</div>
+            <div>{Competition.division}</div>
+        </div>
+    )
+}
+
 export default function admin() {
     return (
     <main>
-        <div>hello world test</div>
+        <CompetitionCard/>
     </main>
     )
 }
